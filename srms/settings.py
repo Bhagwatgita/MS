03275b95c0dfc7888fe39fdc859e4729f8cdf1b4
Rabindra <rabindradahal2074@gmail.com>
2019-11-08 14:46:29 +0545

    Issue Improvised

--- srms/settings.py
+++ srms/settings.py
@@ -1,15 +1,21 @@
 import os
 import dj_database_url
 
+# Base directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Secret Key for project ..Highly Confidential
 SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
+# Debug Status SET it to false in LIVE Or in PRODUCTION
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+# Allowed Host Configuration
 
+ALLOWED_HOSTS = ['127.0.0.1','localhost',]
+
+# Installed Application
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -27,6 +33,7 @@ INSTALLED_APPS = [
     'results',
 ]
 
+# Middleware provided by django for easyness to programmer 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -37,8 +44,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# Root URL for project
 ROOT_URLCONF = 'srms.urls'
 
+# templates used
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -57,6 +66,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'srms.wsgi.application'
 
+
+# SQLite Database Specification
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -67,6 +78,7 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+# Password Validators
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -82,10 +94,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+# Language
 LANGUAGE_CODE = 'en-us'
 
+# Time Zone
 TIME_ZONE = 'UTC'
+
+#Internationalization
 
 USE_I18N = True
 
@@ -93,13 +108,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+# static files
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'SRMS/static_cdn')
+
+#Redirect URL
 
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'home'
